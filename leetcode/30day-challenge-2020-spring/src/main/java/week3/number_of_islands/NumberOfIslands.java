f8d38653178769c690d0d7e42a5425c7b5d12c82
Oleg Koskin <oleg.koskin@gmail.com>
2020-04-27 10:31:33 +0300

    Clean up

--- leetcode/30day-challenge-2020-spring/src/main/java/week3/number_of_islands/NumberOfIslands.java
+++ leetcode/30day-challenge-2020-spring/src/main/java/week3/number_of_islands/NumberOfIslands.java
@@ -5,11 +5,11 @@ import java.util.Queue;
 
 /**
  * Number of Islands.
- *
+ * <p>
  * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water
  * and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are
  * all surrounded by water.
- *
+ * <p>
  * Example 1:
  * Input:
  * 11110
@@ -17,7 +17,7 @@ import java.util.Queue;
  * 11000
  * 00000
  * Output: 1
- *
+ * <p>
  * Example 2:
  * Input:
  * 11000
@@ -28,10 +28,7 @@ import java.util.Queue;
  */
 public class NumberOfIslands {
 
-    private static final int[] DX = {1,0,0,1};
-    private static final int[] DY = {0,1,-1,0};
-
-    private static final int[][] DIRS =  {{-1,0},{1,0},{0,-1},{0,1}};
+    private static final int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     /**
      * "Counting the number of connected components in an undirected graph" problem.
@@ -40,78 +37,51 @@ public class NumberOfIslands {
      * @return number of islands
      */
     public int numIslands(char[][] grid) {
-        /*if(grid==null || grid.length==0) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
 
-        int rowAmount = grid.length;
-        int colAmount = grid[0].length;
-        boolean[][] visited = new boolean[rowAmount][colAmount];
-        int count = 0;
-        for (int i = 0; i < rowAmount; i++) {
-            for (int j = 0; j < colAmount; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    DFS(grid, i, j, visited);
-                    count++;
-                }
-            }
-        }
-        return count;*/
-        if(grid==null || grid.length==0) return 0;
         int islands = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
                     islands++;
-                    BFS(grid,i,j);
+                    BFS(grid, i, j);
                 }
             }
         }
         return islands;
     }
 
-    /*private boolean isSafe(char[][] grid, int row, int col, boolean[][] visited) {
-        return row >=0 && row < visited.length
-                && col >= 0 && col < visited[0].length
-                && grid[row][col] == 1 && !visited[row][col];
-    }
-
-    private void DFS(char[][] grid, int row, int col, boolean[][] visited) {
-        visited[row][col] = true;
-
-        for(int k = 0; k < 4; k++) {
-            if (isSafe(grid, row + DX[k], col + DY[k],visited)) {
-                DFS(grid, row + DX[k], col + DY[k],visited);
-            }
-        }
-    }*/
-
-    private void BFS(char[][] grid, int x, int y){
+    private void BFS(char[][] grid, int x, int y) {
         grid[x][y] = '0';
-        Queue<Point> q = new LinkedList<Point>();
-        q.offer(new Point(x,y));
-        while(q.size()>0){
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
+
+        while (!q.isEmpty()) {
             int size = q.size();
             Point p = q.poll();
-            for(int i=0;i<size;i++){
-                for(int[] dir: DIRS){
-                    int x1 = p.x+dir[0];
-                    int y1 = p.y+dir[1];
-                    if(x1>=0 && y1>=0 && x1< grid.length && y1<grid[0].length && grid[x1][y1]=='1'){
+
+            for (int i = 0; i < size; i++) {
+                for (int[] dir : DIRS) {
+                    int x1 = p.x + dir[0];
+                    int y1 = p.y + dir[1];
+
+                    if (x1 >= 0 && y1 >= 0 && x1 < grid.length && y1 < grid[0].length && grid[x1][y1] == '1') {
                         grid[x1][y1] = '0';
-                        q.offer(new Point(x1,y1));
+                        q.offer(new Point(x1, y1));
                     }
                 }
             }
         }
     }
 
-    static class Point{
+    static class Point {
 
         int x;
         int y;
 
-        Point(int x, int y){
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
